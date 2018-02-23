f59a1deff6c919bb0d9660e9615f14aca6122d34
BourbonCreams <smemo23@gmail.com>
2018-02-23 21:27:24 +0100

    DB and initial files for week1

---
+++ week1/main.py
@@ -0,0 +1,42 @@
+
+
+#########
+# Task1 #
+#########
+
+# Implement the following metrics: Precision, Recall & F1-Score
+
+
+
+#########
+# Task2 #
+#########
+
+# Temporal analysis:
+# (True Positives and Total Foreground) vs time
+# AND
+# (F1 score) vs time
+
+
+
+#########
+# Task3 #
+#########
+
+# Optical flow evaluation metrics
+
+
+
+#########
+# Task4 #
+#########
+
+# Desynchronized results
+
+
+
+#########
+# Task5 #
+#########
+
+# Visual representation optical flow
