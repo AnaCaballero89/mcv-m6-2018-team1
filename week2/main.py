61e0cfd4b2309849226069e900d6f0e8c42f6a46
BourbonCreams <smemo23@gmail.com>
2018-03-05 13:16:56 +0100

    Start of week2

---
+++ week2/main.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('../week1/')
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from opticalFlowMetrics import opticalFlowMetrics
+from utils import *
+
+
+#########
+# Task1 #
+#########
+
+# Gaussian distribution + evaluation
+task1()
+
+
+#########
+# Task2 #
+#########
+
+# Recursive Gaussian modeling + Evaluate and comparison to non-recursive
+task2()
+
+
+#########
+# Task3 #
+#########
+
+# Comparison with state-of-the-art
+task3()
+
+
+#########
+# Task4 #
+#########
+
+# Color sequences
+task4()
---
+++ week2/task4.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('../week1/')
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from opticalFlowMetrics import opticalFlowMetrics
+from utils import *
