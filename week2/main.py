1271e6385de421c00a1ab2000692de8a9cfbc85c
Santiago Barbarisi <santiagoba88@poblenou-131-71.eduroamcp.upf.edu>
2018-03-07 15:51:10 +0100

    update

--- week2/main.py
+++ week2/main.py
@@ -7,8 +7,8 @@ import os
 from utils import *
 from task1 import task1
 from task2 import task2
-#from task3 import task3
-#from task4 import task4
+from task3 import task3
+from task4 import task4
 
 dataset = 'highway'  # 'fall', 'highway', 'traffic'
 
