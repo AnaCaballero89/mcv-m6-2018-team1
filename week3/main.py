c4640f977011f9644ef486f179aa228cb748b8a2
BourbonCreams <smemo23@gmail.com>
2018-03-13 17:10:36 +0100

    Updated automatic thresholds for different datasets in the main file

--- week3/main.py
+++ week3/main.py
@@ -12,7 +12,7 @@ from task4 import task4
 from task5 import task5
 
 
-dataset = 'highway'  # 'fall', 'highway', 'traffic'
+dataset = 'traffic'  # 'fall', 'highway', 'traffic'
 
 inputpath='../datasets/' + dataset + '/input/'
 groundTruthPath='../datasets/' + dataset + '/groundtruth/'
@@ -22,21 +22,27 @@ if dataset == 'highway':
     tr_frmEnd = 1200
     te_frmStart = 1201
     te_frmEnd = 1350
+    MOGthreshold = 10
 
 elif dataset == 'fall':
     tr_frmStart = 1460
     tr_frmEnd = 1510
     te_frmStart = 1511
     te_frmEnd = 1560
+    MOGthreshold = 190
 
 elif dataset == 'traffic':
     tr_frmStart = 950
     tr_frmEnd = 1000
     te_frmStart = 1001
     te_frmEnd = 1050
+    MOGthreshold = 330
+else:
+    print "You haven't defined the right dataset. Options are: highway, fall or traffic."
+    exit(0)
 
 
-mog2BG = cv2.BackgroundSubtractorMOG2(history=150, varThreshold=10, bShadowDetection = False)
+mog2BG = cv2.BackgroundSubtractorMOG2(history=150, varThreshold=MOGthreshold, bShadowDetection = False)
 
 groundTruthImgs = readGT(groundTruthPath, te_frmStart, te_frmEnd)
 
